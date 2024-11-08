1f60be267ef7725201cdaf5ee4cd470eadd3b026
Ilia Popovsky <popovskyilia@gmail.com>
2024-11-08 22:04:48 +0300

    first version project1

--- Parsing_HTTP_server_logs.c
+++ Parsing_HTTP_server_logs.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    DIR *dp = NULL;
+    //dp = opendir(argv[1]);
+    dp = opendir("/home/ilia/OTUS_HW_12/logs");
     printf("Hello world!\n");
     return 0;
 }
