c81838b11f4ab0da05ba616deb37e49bf597a5a1
Ilia Popovsky <popovskyilia@gmail.com>
2024-11-09 21:58:04 +0300

    first version project2

--- Parsing_HTTP_server_logs.c
+++ Parsing_HTTP_server_logs.c
@@ -4,9 +4,20 @@
 
 int main(int argc, char *argv[])
 {
+
     DIR *dp = NULL;
+    struct dirent *dirp;
     //dp = opendir(argv[1]);
     dp = opendir("/home/ilia/OTUS_HW_12/logs");
-    printf("Hello world!\n");
+    if(dp == NULL)
+    {
+       printf("Не удалось открыть каталог %s\n", "/home/ilia/OTUS_HW_12/logs");
+       exit(1);
+    }
+    while((dirp = readdir(dp)) != NULL)
+    {
+       printf("%s\n", dirp->d_name);
+    }
+    printf("\nHello world!\n");
     return 0;
 }
