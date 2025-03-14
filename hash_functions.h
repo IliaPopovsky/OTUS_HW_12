3985f99d91e8935a0f54fc4f462c18b792bf622d
Ilia Popovsky <popovskyilia@gmail.com>
2025-03-14 17:54:21 +0300

    second version project7

---
+++ hash_functions.h
@@ -0,0 +1,25 @@
+#ifndef HASH_FUNCTIONS_H_INCLUDED
+#define HASH_FUNCTIONS_H_INCLUDED
+
+#define WORDLEN 25
+
+typedef struct item{
+   char word[WORDLEN];
+   int quantity;
+} Item;
+
+
+typedef struct HTnode{
+   Item item;
+} HTnode;
+
+
+long long int hash_function(const char *string, long long int size_array);
+long long int hash_function1(const char *string, long long int size_array);
+long long int hash_function2(const char *string, long long int size_array);
+long long int hash_function3(const char *string, long long int size_array);
+long long int hash_function4(const char *string, long long int size_array);
+long long int hash_function5(const char *string, long long int size_array);
+
+
+#endif // HASH_FUNCTIONS_H_INCLUDED
