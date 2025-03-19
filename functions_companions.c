09362e47b5ebef9d012a1c01953213918da1e30b
Ilia Popovsky <popovskyilia@gmail.com>
2025-03-19 18:33:17 +0300

    second version project8

--- functions_companions.c
+++ functions_companions.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-void parsing_string(FILE *fp);
+
+#define SIZE 8192
+#define SIZE_BYTES_BUF 30
+
+long parsing_string(char *str, char *url, char *referer);
 
 void *thread_body(void *arg)
 {
+
+    int number_thread = *((int *) arg);
+	int fd = 0;
+	int length = 0;
+
+    char url[SIZE] = { 0 };
+	char referer[SIZE] = { 0 };
+	char str[SIZE] = {0};
     long last = 0;                                  // размер файла
     fseek((FILE *)arg, 0L, SEEK_SET);               // перейти в начало файла
     fseek((FILE *)arg, 0L, SEEK_END);               // перейти в конец файла
     last = ftell((FILE *)arg);
     printf("Размер файла %ld байт\n", last);
     fseek((FILE *)arg, 0L, SEEK_SET);               // перейти в начало файла
-    parsing_string((FILE *)arg);
+
+
+
+
+    parsing_string(str, url, referer);
 }
