63421b8d46b2d8679852758b1a5b9ad993046c04
Ilia Popovsky <popovskyilia@gmail.com>
2025-04-30 13:29:21 +0300

    first version project

--- Parsing_HTTP_server_logs.c
+++ Parsing_HTTP_server_logs.c
@@ -1,117 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
 #include <string.h>
+#include <dirent.h>
+#include <glib.h>
+#include <glib/gprintf.h>
+#include <pcre.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <pthread.h>
+#include <assert.h>
+#include "hash_table.h"
+#include "parse_functions.h"
+#include "thread_data.h"
 
-char name_file[400] = {0};
+char save_pwd[PWD_LENGTH] = {0};
+thread_data *thread_data_array = NULL;
 
-void *thread_body(void *arg);
-void parsing_string(FILE *fp);
-
-int main(int argc, char *argv[])
+int main (int argc, char **argv)
 {
+	if (argc != 3) {
+		print_error ("Use: app log_dir count_threads.\n");
+	}
 
-    DIR *dp = NULL;
-    FILE *fp = NULL;
-    struct dirent *dirp = NULL;
-    long int location = 0;
-    //dp = opendir(argv[1]);
-    char name_directory[100] = {0};
-    //char name_file[400] = {0};
-    int read_symbol = 0;
-    int number_files = 0;
-    int number_threads = 8;
-    pthread_t thread[number_threads];
-    int result[number_threads];
+	/* сохраним текущий каталог для возврата */
+	if (getcwd (save_pwd, PWD_LENGTH) == NULL) {
+		print_error ("ошибка вызова getcwd");
+	}
 
-    strcpy(name_directory, "/home/ilia/OTUS_HW_12/logs");
-    //dp = opendir("/home/ilia/OTUS_HW_12/logs");
-    dp = opendir(name_directory);
-    if(dp == NULL)
-    {
-       printf("Не удалось открыть каталог %s\n", "/home/ilia/OTUS_HW_12/logs");
-       exit(1);
-    }
-    location = telldir(dp);
-    while((dirp = readdir(dp)) != NULL)
-    {
-       if(strcmp(dirp->d_name, ".") != 0 && strcmp(dirp->d_name, "..") != 0)
-            number_files++;
-       printf("%s\n", dirp->d_name);
-    }
-    printf("В каталоге %s %d файлов.\n", name_directory, number_files);
-    printf("И снова вывод!\n");
-    FILE *file_pointers[number_files];  // Количество файловых дескрипторов, совпадает с количеством имен файлов
-    // Открываем файлы в каталоге.
-    seekdir(dp, location);
-    for(int i = 0; (dirp = readdir(dp)) != NULL && i < number_files;)
-    {
-       if(strcmp(dirp->d_name, ".") != 0 && strcmp(dirp->d_name, "..") != 0)
-       {
-          sprintf(name_file, "%s/%s", name_directory, dirp->d_name);
-          if((file_pointers[i] = fopen(name_file, "rb")) == NULL)
-          {
-             printf("Не удалось открыть файл %s\n", name_file);
-             //exit(EXIT_FAILURE);
-          }
-          printf("%s\n", dirp->d_name);
-          for(int j = 0; j < 400; j++)
-          {
-             read_symbol = getc(file_pointers[i]);
-             putc(read_symbol, stdout);
-          }
-          i++;
-       }
+	unsigned int count_threads = atoi(argv[2]);
+	if (count_threads < 1 || count_threads > 50) {
+		count_threads = 1;
+	}
 
-    }
-    // Создаем потоки из разчета 1 поток на 1 файл
-    for(int i = 0; i < number_files; i++)
-    {
-       result[i] = pthread_create(&thread[i], NULL, thread_body, file_pointers[i]);
-       if(result[i])
-       {
-          printf("The threads could not be created\n");
-          exit(1);
-       }
-       result[i] = pthread_join(thread[i], NULL);
-       if(result[i])
-       {
-          printf("The threads could not be joined\n");
-          exit(2);
-       }
-    }
+	thread_data_array = new_thread_data(count_threads);
 
-    seekdir(dp, location);
-    // Чтение файлов каталога.
-    if((dirp = readdir(dp)) == NULL)
-    {
-       printf("Не удалось прочитать каталог!\n");
-    }
-    sprintf(name_file, "%s/%s", name_directory, dirp->d_name);
-    if((fp = fopen(name_file, "rb")) == NULL)
-    {
-       printf("Не удалось открыть файл %s\n", name_file);
-       //exit(EXIT_FAILURE);
-    }
-    if((fp = fopen(dirp->d_name, "rb")) == NULL)
-    {
-       printf("Не удалось открыть файл %s\n", dirp->d_name);
-       //exit(EXIT_FAILURE);
-    }
+	DIR *dp;
+	if ((dp = opendir(argv[1])) == NULL) {
+		print_error("невозможно открыть %s для чтения", argv[1]);
+	}
 
-    if((fp = fopen(name_file, "rb")) == NULL)
-    {
-       printf("Не удалось открыть файл %s\n", name_file);
-       //exit(EXIT_FAILURE);
-    }
-    for(int i = 0; i < 500; i++)
-    {
-       read_symbol = getc(fp);
-       putc(read_symbol, stdout);
-    }
-    for(int i = 0; i < number_files; i++)
-        fclose(file_pointers[i]);
-    printf("\nHello world!\n");
-    return 0;
+	/*распределение файлов по потокам */
+	int threadNum = 0;
+	struct dirent *dirp;
+	while ((dirp = readdir (dp)) != NULL) {
+		if (strcmp (dirp->d_name, ".") == 0 ||
+				strcmp (dirp->d_name, "..") == 0) {
+			continue;
+		}
+
+		add_file_path_thread_data(thread_data_array,
+								   threadNum++,
+								   count_threads,
+								   dirp->d_name);
+	}
+
+	closedir(dp);
+
+	/*work threads */
+	/* сменим каталог на каталог с логами, чтобы потоки нашли свои файлы */
+	if (chdir (argv[1]) < 0) {
+		print_error("ошибка вызова функцииsave_pwd для каталога %s\n", argv[1]);
+	}
+
+	for (unsigned int i = 0; i < count_threads; i++) {
+		int ans = pthread_create (&thread_data_array[i].thread_id,
+								NULL,
+								parse_logs,
+								(void *) &thread_data_array[i].num);
+		if (ans != 0) {
+			print_error ("невозможно создать поток %d\n",  i);
+		}
+	}
+
+
+	void *tret;
+	for (unsigned int i = 0; i < count_threads; i++) {
+		if (pthread_join (thread_data_array[i].thread_id, &tret) != 0)  {
+			print_error("невозможно присоединить поток %d", i);
+		}
+	}
+
+	/* вернем каталог в прежнее положение */
+	if (chdir (save_pwd) < 0) {
+		print_error("ошибка вызова функции chdir для каталога %s\n", save_pwd);
+	}
+
+	/* объединяем все данные в последнуюю структуру выводим данные и очищаем все */
+	union_hash(thread_data_array, count_threads);
+
+	printf("общее количество отданных байт = %zu\n", thread_data_array[count_threads].byte_count);
+
+	printf("10 самых “тяжёлых” по суммарному трафику URL’ов:\n");
+	print_top_by_value(thread_data_array[count_threads].url_counter, 10);
+
+	printf("10 наиболее часто встречающихся Referer’ов:\n");
+	print_top_by_value (thread_data_array[count_threads].ref_counter, 10);
+
+	free_thread_data (thread_data_array, count_threads);
+
+	return EXIT_SUCCESS;
+
 }
+
